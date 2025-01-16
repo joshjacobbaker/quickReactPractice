ffb953855448d0e927f7018268bd34f162f7f900
Josh Baker <josh.jacob.baker@gmail.com>
2025-01-16 13:20:56 -0800

    testingagain

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ const App: React.FC = () => {
           alignItems: 'center',
         }}
       >
-        <div>asdf</div>
         <nav>
           <ul style={{ listStyleType: 'none', padding: 0 }}>
             <li style={{ margin: '10px' }}>
